055f0a5b42fdc73b8f128f0f72cf4b3fd10e355d
Maria Belen Cruzado Montenegro <102304951+Bevyxaa@users.noreply.github.com>
2024-06-10 22:35:53 -0500

    transaction

--- src/app/component/User/TransactionVoucher/transactionvoucher-crear/transactionvoucher-crear.component.ts
+++ src/app/component/User/TransactionVoucher/transactionvoucher-crear/transactionvoucher-crear.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { TransactionVoucherService } from './../../../../services/TransactionVoucher/transaction-voucher.service';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TransactionVoucher } from '../../../../model/TransactionVoucher/transaction-voucher';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-transactionvoucher-crear',
   templateUrl: './transactionvoucher-crear.component.html',
   styleUrl: './transactionvoucher-crear.component.css'
 })
-export class TransactionvoucherCrearComponent {
+export class TransactionvoucherCrearComponent implements OnInit{
+  
+  form: FormGroup = new FormGroup({});
+  transactionvoucher: TransactionVoucher = new TransactionVoucher();
+  mensaje: string = '';
+  id: number = 0;
+
+  constructor(
+    private TransactionVoucherService: TransactionVoucherService,
+    private router: Router,
+    private route : ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((data:Params)=>
+      {
+        this.id = data['id']; 
+        this.init();
+       });
+  
+      this.form = new FormGroup({
+        id: new FormControl(),
+        total_amount: new FormControl('', [Validators.required]),
+      })
+  }
+
+  init() {
+    this.TransactionVoucherService.list().subscribe((data) => {
+     this.form = new FormGroup({
+      id: new FormControl(data.id),
+      total_amount: new FormControl(data.total_amount),
+     });
+     });
+    }
+    
+    aceptar(){
+      this.transactionvoucher.id = this.form.value['id'];
+      this.transactionvoucher.total_amount = this.form.value['total_amount'];
+  
+      if (this.form.valid) {
+        console.log(this.transactionvoucher);
+        this.TransactionVoucherService.insert(this.transactionvoucher).subscribe(() => {
+          this.TransactionVoucherService.list().subscribe((data: TransactionVoucher[]) => {
+            this.TransactionVoucherService.setList(data); 
+          });
+        });
+        this.router.navigate(['/TransactionVoucher/findAll/listar']);
+      } else {
+        this.mensaje = "Agregue campos omitidos";
+      }
+    }
 
 }
--- src/app/services/TransactionVoucher/transaction-voucher.service.ts
+++ src/app/services/TransactionVoucher/transaction-voucher.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+import { TransactionVoucher } from '../../model/TransactionVoucher/transaction-voucher';
+import { environment } from '../../../environments/environments';
+const base_url = environment.base
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionVoucherService {
+  private url = `${base_url}` 
+  private listaCambio = new Subject<TransactionVoucher[]>()
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  list() : Observable<any>{
+    return this.http.get<TransactionVoucher[]> (this.url + "/TransactionVoucher/findAll");
+  }
+  listId(id:number){
+    return this.http.get<TransactionVoucher>(this.url+"/TransactionVoucher/firstByUser/"+id);
+  }
+  insert(transactionvoucher:TransactionVoucher){
+    return this.http.post(this.url+ '/TransactionVoucher/save', transactionvoucher);
+  }
+  update(tv: TransactionVoucher){
+    return this.http.put(this.url + "/TransactionVoucher/update", tv);
+  }
+  delete(id:string){
+    return this.http.delete(this.url + "/TransactionVoucher/delete/" + id);
+  }
+  setList(listaNueva : TransactionVoucher[]){
+    this.listaCambio.next(listaNueva);
+  }
+  getList(){
+    return this.listaCambio.asObservable();
+  }
 }

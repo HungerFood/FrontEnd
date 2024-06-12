5f98a0d59f501190aa64923647eb62647a68f132
Maria Belen Cruzado Montenegro <102304951+Bevyxaa@users.noreply.github.com>
2024-06-12 16:55:45 -0500

    OK
    //

--- src/app/component/Login/login/login.component.ts
+++ src/app/component/Login/login/login.component.ts
@@ -1,4 +1,8 @@
+import { LoginService } from './../../../services/Login/login.service';
 import { Component } from '@angular/core';
+import { Credentials } from '../../../model/Login/Credencials/credencials';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  creds: Credentials= {
+    username: "",
+    password: ""
+ }
+
+ constructor(private LoginService: LoginService,
+  private router: Router){};
+
+getAuthors(form: NgForm){
+console.log('Form Value:',form.value);
+return this.LoginService.login(this.creds)
+                .subscribe(response => {
+                this.router.navigate(['/authors']);
+                })
+}
+
+login(form: NgForm){
+  console.log('form value', form.value);
+   this.LoginService.login(this.creds)
+    .subscribe(response => {
+      this.router.navigate(['/authors']);
+    });
+}
 
 }

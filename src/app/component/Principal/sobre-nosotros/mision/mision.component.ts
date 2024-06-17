e5be041328781844ca5ff8357e9ff823de8dd1ef
Alexander Josue Suclupe Paucar <u20211b506@upc.edu.pe>
2024-06-16 20:09:07 -0500

    actualizado

--- src/app/component/Principal/sobre-nosotros/mision/mision.component.ts
+++ src/app/component/Principal/sobre-nosotros/mision/mision.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mision',
   templateUrl: './mision.component.html',
-  styleUrl: './mision.component.css'
+  styleUrls: ['./mision.component.css']
 })
-export class MisionComponent {
+export class MisionComponent implements OnInit {
+
+  isVisible: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.checkScroll(); // Verificar el desplazamiento inicial
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const scrollPosition = window.pageYOffset + window.innerHeight;
+    const elementPosition = (document.querySelector('.value-section') as HTMLElement)?.offsetTop || 0;
+    const elementHeight = (document.querySelector('.value-section') as HTMLElement)?.offsetHeight || 0;
+
+    // Calcula la posición de inicio y fin de la sección de misión
+    const startOffset = elementPosition - window.innerHeight;
+    const endOffset = elementPosition + elementHeight;
+
+    if (scrollPosition > startOffset && scrollPosition < endOffset) {
+      this.isVisible = true;
+    } else {
+      this.isVisible = false;
+    }
+  }
 
 }
--- src/app/component/Principal/sobre-nosotros/vision/vision.component.ts
+++ src/app/component/Principal/sobre-nosotros/vision/vision.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-vision',
   templateUrl: './vision.component.html',
-  styleUrl: './vision.component.css'
+  styleUrls: ['./vision.component.css']
 })
 export class VisionComponent {
+  @ViewChild('valueSection') valueSection!: ElementRef; // Referencia a la sección principal
 
+  constructor(private renderer: Renderer2) {}
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll(): void {
+    const componentPosition = this.valueSection.nativeElement.offsetTop;
+    const scrollPosition = window.pageYOffset + window.innerHeight;
+
+    if (scrollPosition > componentPosition) {
+      this.renderer.addClass(this.valueSection.nativeElement, 'visible');
+    } else {
+      this.renderer.removeClass(this.valueSection.nativeElement, 'visible');
+    }
+  }
 }

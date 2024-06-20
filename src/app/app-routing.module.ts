e3d31b7e1c8fdb6c3fa8ce0ecd4ff71aab7736f5
Alexander Josue Suclupe Paucar <u20211b506@upc.edu.pe>
2024-06-19 22:24:40 -0500

    implemente lo de chldren y mejoras en el navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,83 +20,57 @@ import { PoliticaDePrivacidadComponent } from './component/Footer/Politica-De-Pr
 import { TerminosYCondicionesComponent } from './component/Footer/Terminos-Y-Condiciones/terminos-y-condiciones/terminos-y-condiciones.component';
 import { LibroDeReclamosComponent } from './component/Footer/Libro-De-Reclamos/libro-de-reclamos/libro-de-reclamos.component';
 import { PoliticaDeCookiesComponent } from './component/Footer/Politica-De-Cookies/politica-de-cookies/politica-de-cookies.component';
-;
 
 const routes: Routes = [
-  
-  //------------------------------------------------------------------RUTA POR DEFECTO----------------------------------------------------------------------------------------------
-  { path: '', redirectTo: 'principal/home', pathMatch: 'full' }, // Ruta por defecto
-
-  //------------------------------------------------------------------PRINCIPAL----------------------------------------------------------------------------------------------
-  { path: 'principal/home', component: MAINComponent },
-
-  //------------------------------------------------------------------NAVBAR DONANTE----------------------------------------------------------------------------------------------
-
-
-  //------------------------------------------------------------------NAVBAR ADMIN----------------------------------------------------------------------------------------------
-
-
-  //------------------------------------------------------------------NAVBAR PRINCIPAL----------------------------------------------------------------------------------------------
-  { path: 'principal/sobre-nosotros', component: SobreNosotrosComponent },// SOBRE NOSOTROS
-  { path: 'principal/donar', component: DonarComponent },//DONAR
-  { path: 'principal/contactenos', component: ContactenosComponent },// CONTACTENOS
-  { path: 'principal/hola-inicia-sesion', component: HolaIniciaSesionComponent },// HOLA INICIA SESION
-
-
-  //------------------------------------------------------------------LOGIN----------------------------------------------------------------------------------------------
-  { path: 'principal/login', component: LoginComponent },// INICIAR SESION
-
-  //------------------------------------------------------------------REGISTRARSE----------------------------------------------------------------------------------------------
-  { path: 'principal/registrarse', component: RegistrarseComponent },// REGISTRARSE
-
-  //------------------------------------------------------------------PERFIL----------------------------------------------------------------------------------------------
-  { path: 'principal/perfil', component: PerfilComponent },// PERFIL PRINCIPAL, DESDE AQUI NAVEGA A TODOS LADOS
-
-  //------------------------------------------------------------------PERFIL DONANTE----------------------------------------------------------------------------------------------
-  { path: 'principal/perfilListar', component: PerfilListarComponent },// MUESTRA LOS DATOS BASICOS DEL DONANTE/USUARIO
-
-
-  
-  
+  { path: '', component: MAINComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registrarse', component: RegistrarseComponent },
+  { path: 'contactenos', component: ContactenosComponent },
+  { path: 'donar', component: DonarComponent },
+  { path: 'hola-inicia-sesion', component: HolaIniciaSesionComponent },
+  { path: 'sobre-nosotros', component: SobreNosotrosComponent },
   
 
+  //------------------------------------------------- PRINCIPAL -------------------------------------------------
+  {
+    path: 'principal',
+    children: [
+      { path: 'donacionesDineroCrear', component: MoneyDonationCrearComponent },
+      { path: 'donacionesDineroEditar/:id', component: MoneyDonationCrearComponent },
+      { path: 'donacionesDineroListar', component: MoneyDonationComponent },
+      { path: 'donacionesComidaCrear', component: FooddonationCrearComponent },
+      { path: 'donacionesComidaEditar/:id', component: FooddonationCrearComponent },
+      { path: 'donacionesComidaListar', component: FooddonationComponent },
+    ],
+  },
 
-  //------------------------------------------------------------------DONANTE----------------------------------------------------------------------------------------------
-    //DONACIONES DE DINERO
-  { path: 'principal/donacionesDineroCrear', component: MoneyDonationCrearComponent },// REGISTRAR DONACIONES DE DINERO
-  { path: 'principal/donacionesDineroEditar/:id', component: MoneyDonationCrearComponent },// EDITAR DONACIONES DE DINERO
-  { path: 'principal/donacionesDineroListar', component: MoneyDonationComponent },// MUESTRA Y LISTA LAS DONACIONES DE DINERO
-  
-      //DONACIONES DE COMIDA
-  { path: 'principal/donacionesComidaCrear', component: FooddonationCrearComponent },// REGISTRAR DONACIONES DE COMIDA
-  { path: 'principal/donacionesComidaEditar/:id', component: FooddonationCrearComponent },// EDITAR DONACIONES DE COMIDA
-  { path: 'principal/donacionesComidaListar', component: FooddonationComponent },// MUESTRA Y LISTA LAS DONACIONES DE COMIDA
+  {
+    path: 'admin',
+    children: [
+      { path: 'TypeOfFood/findAll/listar', component: TypeOfFoodListarComponent },
+      { path: 'TypeOfFood/findAll/nuevo', component: TypeOfFoodCrearComponent },
+      { path: 'TypeOfFood', component: TypeOfFoodComponent },
+    ],
+  },
 
+  {
+    path: 'user',
+    children: [
+      { path: 'perfil/listar', component: PerfilListarComponent },
+      { path: 'perfil/:id', component: PerfilComponent },
+    ],
+  },
 
-
-
-  //------------------------------------------------------------------ADMIN----------------------------------------------------------------------------------------------
-  { path: 'TypeOfFood/findAll/listar', component: TypeOfFoodListarComponent },
-  { path: 'TypeOfFood/findAll/nuevo', component: TypeOfFoodCrearComponent },
-  { path: 'TypeOfFood', component: TypeOfFoodComponent },
-
-
-
-
-
-
-
-
-
-
-  //------------------------------------------------------------------FOOTER----------------------------------------------------------------------------------------------
-  { path: 'footer/terminos-y-condiciones', component: TerminosYCondicionesComponent },// TERMINOS Y CONDICIONES
-  { path: 'footer/libro-de-reclamaciones', component: LibroDeReclamosComponent },// LIBRO DE RECLAMACIONES
-  { path: 'footer/politica-de-cookies', component: PoliticaDeCookiesComponent },// POLITICA DE COOKIES
-  { path: 'footer/politica-de-privacidad', component: PoliticaDePrivacidadComponent },// POLITICA DE PRIVACIDAD
-  
+  {
+    path: 'footer',
+    children: [
+      { path: 'terminos-y-condiciones', component: TerminosYCondicionesComponent },
+      { path: 'libro-de-reclamaciones', component: LibroDeReclamosComponent },
+      { path: 'politica-de-cookies', component: PoliticaDeCookiesComponent },
+      { path: 'politica-de-privacidad', component: PoliticaDePrivacidadComponent },
+    ],
+  },
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
